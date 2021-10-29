6d7ffb408c7ed240578927351278f80b9f56613e
Janu Lingeswaran <Janusan@Lingeswaran.de>
2021-10-29 20:53:45 +0000

    Attack

--- main.py
+++ main.py
@@ -24,27 +24,48 @@ class Fighter:
   def update(self,map):
       map.update(self.x,self.y,self.id)
   def heal(self):
-        self.temp = self.life
-  def left(self, map):
+      self.temp = self.life
+  def attack(self, fighters,x,y):
+      for fighter in fighters:
+        if fighter.x==x and fighter.y==y:
+          victim = fighter
+      minPower = round(self.strength*0.7,0)
+      maxPower = round(self.strength*1.3,0)
+      victim.temp -= round(random.randint(minPower, maxPower)/10,2)
+      victim.temp = round(victim.temp,2)
+      print("KABOOM!")
+
+  def left(self, map, fighters):
     if not self.x - 1 < 0 and map.map[self.x-1][self.y] == 0:
       self.delete(map)
       self.x -= 1
       self.update(map)
-  def right(self, map):
-     if not self.x + 1 == map.x and map.map[self.x+1][self.y] == 0:
+    elif not self.x - 1 < 0 and not map.map[self.x-1][self.y] == 0:
+      self.attack(fighters,self.x-1,self.y)
+
+  def right(self, map, fighters):
+    if not self.x + 1 == map.x and map.map[self.x+1][self.y] == 0:
       self.delete(map)
       self.x += 1
       self.update(map)
-  def up(self, map):
+    elif not self.x + 1 == map.x and not map.map[self.x+1][self.y] == 0:
+      self.attack(fighters,self.x+1,self.y)
+
+  def up(self, map, fighters):
     if not self.y - 1 < 0 and map.map[self.x][self.y-1] == 0:
       self.delete(map)
       self.y -= 1
       self.update(map)
-  def down(self, map):
-     if not self.y + 1 == map.y and map.map[self.x][self.y+1] == 0:
+    elif not self.y - 1 < 0 and not map.map[self.x][self.y-1] == 0:
+      self.attack(fighters,self.x,self.y-1)
+
+  def down(self, map, fighters):
+    if not self.y + 1 == map.y and map.map[self.x][self.y+1] == 0:
       self.delete(map)
       self.y += 1
       self.update(map)
+    elif not self.y + 1 == map.y and not map.map[self.x][self.y+1] == 0:
+      self.attack(fighters,self.x,self.y+1)
 
 def simulate_game(map,fighters):
   for fighter in fighters:
@@ -61,7 +82,7 @@ def simulate_game(map,fighters):
   WHITE    = ( 255, 255, 255)
   # GREEN    = (   0, 255,   0)
   # RED      = ( 255,   0,   0)
-  # BLUE     = (   0,   0, 255)
+  BLUE     = (   0,   0, 255)
   pygame.init()
   size = (700, 500)
   screen = pygame.display.set_mode(size)  
@@ -81,20 +102,26 @@ def simulate_game(map,fighters):
       for fighter in fighters:
         where = random.randint(1,4)
         if where == 1:
-          fighter.up(map)
+          fighter.up(map, fighters)
         elif where == 2:
-          fighter.down(map)
+          fighter.down(map, fighters)
         elif where == 3:
-          fighter.right(map)
+          fighter.right(map, fighters)
         elif where == 4:
-          fighter.left(map)
+          fighter.left(map, fighters)
       # --- Drawing code should go here
-      for x in range(0,len(map.map)):
-        for y in range(0,len(map.map[x])):
+      for x in range(0,map.x):
+        for y in range(0,map.y):
           font = pygame.font.SysFont('Calibri', 25, True, False)
           if map.map[x][y] != 0:
             text = font.render(str(map.map[x][y]),True,BLACK)
-            screen.blit(text, [x*40, y*40])          
+            screen.blit(text, [x*40, y*40])
+      fighterCounter = 0  
+      for fighter in fighters:
+        font = pygame.font.SysFont('Calibri', 15, True, False)
+        text = font.render(fighter.name+", "+str(fighter.temp)+"/"+str(fighter.life), True, BLUE)
+        screen.blit(text, [map.x*40+10, map.y*40+20*fighterCounter])
+        fighterCounter+=1
       pygame.display.update()
       # First, clear the screen to white. Don't put other drawing commands
       # above this, or they will be erased with this command.
@@ -104,7 +131,7 @@ def simulate_game(map,fighters):
       clock.tick(5)
 
 def main():
-  map = Map(5,5)
+  map = Map(5,2)
   otto = Fighter("Otto", 93, 93, 93, "O")
   hans = Fighter("Hans", 94, 94, 94, "H")
   simulate_game(map,[otto,hans])
