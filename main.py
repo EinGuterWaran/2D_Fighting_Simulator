56dbade2e29c1805e2cbedbe4e1952bbd31f4cf9
Janu Lingeswaran <Janusan@Lingeswaran.de>
2021-10-31 10:49:39 +0000

    When several fighters are knocked out at the same time

--- main.py
+++ main.py
@@ -159,8 +159,10 @@ def simulate_game(map, fighters):
                 done = True  # Flag that we are done so we exit this loop
 
         # --- Game logic should go here
+        died = []
         for fighter in fighters2:
             if fighter.temp <= 0 and fighter in fighters:
+                died.append(fighter)
                 deadText = fighter.name + " is dead."
                 textPipe.append(deadText)
                 death_prints.append([
@@ -205,20 +207,64 @@ def simulate_game(map, fighters):
             for x in stats:
                 x = stats[x]
                 the_text2 = x["name"] + ", KILLS: " + str(
-                    x["kills"]) + ", DAMAGE: " + str(x["damage"])+", HITS: "+str(x["hits"])+", CRITIAL HITS: "+str(x["crit"])
+                    x["kills"]) + ", DAMAGE: " + str(
+                        x["damage"]) + ", HITS: " + str(
+                            x["hits"]) + ", CRITIAL HITS: " + str(x["crit"])
                 print(the_text2)
                 textPipe.append(the_text2)
 
-            fighters[0].delete(map)
-            fighters.remove(fighters[0])
+            # fighters[0].delete(map)
+            # fighters.remove(fighters[0])
             screen.blit(text, [map.x * 40 + 10, 40 * (amount_fig - 1)])
+
+        #When all are dead at the end of the round
+        if len(fighters) == 0:
+            max = None
+            winner = []
+            for deadFig in died:
+                if max == None:
+                    max = deadFig.temp
+                    winner.append(deadFig)
+                elif deadFig.temp == max:
+                    winner.append(deadFig)
+                elif deadFig.temp > max:
+                    winner.clear()
+                    winner.append(deadFig)
+                    max = deadFig.temp
+            done = True
+            font = pygame.font.SysFont('Calibri', 15, True, False)
+            if len(winner) == 1:
+                the_text = winner[
+                    0].name + " was also knocked out but is the first to come back to his senses and wins!!"
+            else:
+                if len(winner) == 2:
+                    howMany = "both"
+                else:
+                    howMany = str(len(winner))
+                the_text = "WOW! What a bizarre situation. " + howMany + " fighters are knocked out at the same time and come back to their senses at the same time. This is a REAL draw between:"
+                text = font.render(the_text, True, GREEN)
+            print(the_text)
+            textPipe.append(the_text)
+            if (len(winner) != 1):
+                for wnr in winner:
+                    print(wnr.name)
+                    textPipe.append(wnr)
+            for x in stats:
+                x = stats[x]
+                the_text2 = x["name"] + ", KILLS: " + str(
+                    x["kills"]) + ", DAMAGE: " + str(
+                        x["damage"]) + ", HITS: " + str(
+                            x["hits"]) + ", CRITIAL HITS: " + str(x["crit"])
+                print(the_text2)
+                textPipe.append(the_text2)
+
         # mode = [textPipeLen, textPipeLen]
         mode = [15, len(textPipe) - 15]
         if len(textPipe) > mode[0] and mode[0] > 0:
             for x in range(0, mode[1]):
                 textPipe.pop(0)
         textCounter = 0
-        textPipeLen = len(textPipe)
+        # textPipeLen = len(textPipe)
         for text in textPipe:
             font = pygame.font.SysFont('Calibri', 10, True, False)
             text2 = font.render(text, True, BLACK)
@@ -230,93 +276,103 @@ def simulate_game(map, fighters):
         screen.fill(WHITE)
         # --- Go ahead and update the screen with what we've drawn.
         # --- Limit to x frames per second
-        clock.tick(100)
+        clock.tick(10)
 
 
 def example(number):
-    map = Map(5, 5)
-    if number==1:
-      otto = Fighter("Otto", 33, 100, 1, 93, "O")
-      hans = Fighter("Hans", 34, 50, 350, 94, "H")
-      peter = Fighter("Peter", 35, 95, 95, 95, "P")
-      walter = Fighter("Walter", 36, 96, 96, 96, "W")
-      # gerd = Fighter("Gerd", 37, 97, 97, 97, "G")
-      # janu = Fighter("Janu", 200, 200, 350,1000, "Janu")
-      simulate_game(map, [otto, hans, peter, walter])
-    elif number==2:
-      janu = Fighter("Janu", 40, 20, 350, 100, "J")
-      damian = Fighter("Damian", 40, 60, 0, 100, "D")
-      simulate_game(map, [janu, damian])
+    map = Map(8, 5)
+    if number == 1:
+        otto = Fighter("Otto", 33, 100, 1, 93, "O")
+        hans = Fighter("Hans", 34, 50, 350, 94, "H")
+        peter = Fighter("Peter", 35, 95, 95, 95, "P")
+        walter = Fighter("Walter", 36, 96, 96, 96, "W")
+        # gerd = Fighter("Gerd", 37, 97, 97, 97, "G")
+        # janu = Fighter("Janu", 200, 200, 350,1000, "Janu")
+        simulate_game(map, [otto, hans, peter, walter])
+    elif number == 2:
+        janu = Fighter("Janu", 50, 20, 350, 100, "J")
+        damian = Fighter("Damian", 50, 60, 0, 100, "D")
+        simulate_game(map, [janu, damian])
 
 
 def main():
     while True:
-      print("Enter e1 if you want to see example game #1 and e2 for example game #2. If you want to set up your own game enter o!")
-      mode = input("")
-      if mode=="e1" or mode=="e2" or mode=="o":
-        break
-      print("That's not e or o!")
-    if mode=="e1":
-      example(1)
-    elif mode=="e2":
-      example(2)
-    elif mode=="o":
-      while True:
-        mapx = input("Map width (10 or higher): ")
-        if str(mapx).isnumeric() and int(mapx) >=10:
-          break
-        print("Please enter a number (10 or higher)!")
-      while True:
-        mapy = input("Map height (5 or higher): ")
-        if str(mapy).isnumeric() and int(mapy) >=5:
-          break
-        print("Please enter a number (5 or higher)!")
-      mapx = int(mapx)
-      mapy = int(mapy)
-      map = Map(mapx,mapy)
-      player_index = 0
-      players=[]
-      while True:
-        while True:
-          name = input("Name of player #"+str(player_index+1)+": ")
-          if type(name) is str:
+        print(
+            "Enter e1 if you want to see example game #1 and e2 for example game #2. If you want to set up your own game enter o!"
+        )
+        mode = input("")
+        if mode == "e1" or mode == "e2" or mode == "o":
             break
+        print("That's not e or o!")
+    if mode == "e1":
+        example(1)
+    elif mode == "e2":
+        example(2)
+    elif mode == "o":
         while True:
-          life = input("Life of "+name+": ")
-          if life.isnumeric():
-            break
+            mapx = input("Map width (8 or higher): ")
+            if str(mapx).isnumeric() and int(mapx) >= 8:
+                break
+            print("Please enter a number (8 or higher)!")
         while True:
-          strength = input("Strength of "+name+": ")
-          if strength.isnumeric():
-            break
+            mapy = input("Map height (5 or higher): ")
+            if str(mapy).isnumeric() and int(mapy) >= 5:
+                break
+            print("Please enter a number (5 or higher)!")
+        mapx = int(mapx)
+        mapy = int(mapy)
+        map = Map(mapx, mapy)
+        player_index = 0
+        players = []
         while True:
-          speed = input("Speed of "+name+": ")
-          if speed.isnumeric():
-            break
-        while True:
-          crit = input("Critical hit of "+name+": ")
-          if crit.isnumeric():
-            break
-        while True:
-          id = input("ID of "+name+": ")
-          if type(id) is str:
-            break
-        players.append([name,int(life),int(strength),int(speed),int(crit),id])
-        while True:
-          end = input("Enter y if you want to add another player. If not, enter n! ")
-          if end=="y" or end=="n":
-            break
-          print("That's not y or n!")
-        if end == "n":
-          break
-        player_index += 1
-      players2=[]
-      for player in players:
-          players2.append(Fighter(player[0], player[1], player[2], player[3],player[4], player[5]))
-      simulate_game(map, players2)
+            while True:
+                name = input("Name of player #" + str(player_index + 1) + ": ")
+                if type(name) is str:
+                    break
+            while True:
+                life = input("Life of " + name + ": ")
+                if life.isnumeric():
+                    break
+            while True:
+                strength = input("Strength of " + name + ": ")
+                if strength.isnumeric():
+                    break
+            while True:
+                speed = input("Speed of " + name + ": ")
+                if speed.isnumeric():
+                    break
+            while True:
+                crit = input("Critical hit of " + name + ": ")
+                if crit.isnumeric():
+                    break
+            while True:
+                id = input("ID of " + name + ": ")
+                if type(id) is str:
+                    break
+            players.append(
+                [name,
+                 int(life),
+                 int(strength),
+                 int(speed),
+                 int(crit), id])
+            while True:
+                end = input(
+                    "Enter y if you want to add another player. If not, enter n! "
+                )
+                if end == "y" or end == "n":
+                    break
+                print("That's not y or n!")
+            if end == "n":
+                break
+            player_index += 1
+        players2 = []
+        for player in players:
+            players2.append(
+                Fighter(player[0], player[1], player[2], player[3], player[4],
+                        player[5]))
+        simulate_game(map, players2)
+
 
 if __name__ == "__main__":
     # example()
-    while True:
-      example(2)
     main()
